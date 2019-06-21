ed1bba9a2f56a7f0c48a202168b5cb2b862097af
Yura <Yura@YuraASUS>
2019-06-22 00:47:47 +0300

    Добавил все лекции

--- src/lection/U06/Task03.java
+++ src/lection/U06/Task03.java
@@ -2,15 +2,15 @@ package lection.U06;
 public class Task03 {
     public static void main(String[] args) {
         // TODO code application logic here
-        charAt();
-        concat();
-        equalsIgnoreCase();
-        replace();
-        substring();
-        toLowerCase();
-        toString();
-        toUpperCase();
-        trim();
+        // charAt();
+        // concat();
+        // equalsIgnoreCase();
+        // replace();
+        // substring();
+        // toLowerCase();
+        // toString();
+        // toUpperCase();
+        // trim();
     }
     
 }
