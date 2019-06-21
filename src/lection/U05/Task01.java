7bb80240e3d5a1249cff407ee17ff3df1e848b58
Yura <Yura@YuraASUS>
2019-06-22 00:07:42 +0300

    Подразгреб лекции

---
+++ src/lection/U05/Task01.java
@@ -0,0 +1,13 @@
+package lection.U05;
+public class Task01 {
+     public static void main(String[] args) {
+        // TODO code application logic here
+      
+    }
+    //Объявление нового метода
+    public static int Summ(int a, int b) {
+        //Возвращает из метода
+        return a + b;
+    }
+
+}
---
+++ src/lection/U05/Task02.java
@@ -0,0 +1,19 @@
+package lection.U05;
+public class Task02 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] array = new int[5];
+        array[0] = 5;
+        array[1] = 12;
+        array[2] = 53;
+        array[3] = 2;
+        array[4] = 36;
+        int i = 0;  
+        do {
+           System.out.println(array[i]);
+           i++;
+           }
+        while (i<=2);
+    }
+
+}
