d40a8a21b6cda0018c7f628e5de774b5d29b0aa2
Devastator1979 <devastator1979@mail.ru>
2019-07-13 11:40:00 +0300

    Еще добавил

--- src/lection11/Task05.java
+++ src/lection11/Task05.java
@@ -3,13 +3,39 @@
  * двоичном коде число.
  */
 package lection11;
-class binary{
-    int a;
-    void b(int a){
-        if (a>0)
-        a=1;
+class Bynary {
+
+    public static void a(int n) {
+        String s;
+        s = "";
+        for (int mask = 1; mask <= n; mask <<= 1) {
+            if ((n & mask) > 0) {
+                System.out.print(1);
+                s = s + "1";
+            } else {
+                System.out.print(0);
+                s = s + "0";
+            }
+        }
+        int stringLength = s.length();
+        String result = "";
+        for (int i = 0; i < stringLength; i++) {
+            result = s.charAt(i) + result;
+        }
+
+        System.out.println();
+        System.out.println("Проверка: " + Integer.toBinaryString(n));
+        System.out.println("Результат в переменной строка: " + s);
+        System.out.println("Перевернутая строка: " + result);
     }
+
 }
+
 public class Task05 {
-    
+
+    public static void main(String[] args) {
+        Bynary bin = new Bynary();
+        bin.a(255866655);
+        
+    }
 }
