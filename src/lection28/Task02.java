f0c69e8833366909976997d50d04f1298f756319
Devastator1979 <Devastator1979@mail.ru>
2019-10-08 16:29:06 +0300

    Доделал коза воза ноза

--- src/lection28/Task02.java
+++ src/lection28/Task02.java
@@ -5,42 +5,9 @@ class Node{
 }
 class List{
     String head;
-    void add(String ref, int date){
-        
-    }
-}
-public class Task02 {
-    public static void main(String args[]){
-        Node head;
-        Node n1 = new Node();
-        head = n1;
-        n1.ref = null;
-        n1.date = 35;
-        Node n2 = new Node();
-        n2.ref = null;
-        n2.date = 48;
-        n1.ref = n2;
-        Node n3 = new Node();
-        n3.ref = null;
-        n3.date = 54;
-        n2.ref = n3;
-        add(100,head);
-        print(head);
-        
-        
-    }
-    static void print(Node head){
+    void add (int date, Node head){
         Node kursor = head;
         while(kursor.ref!=null){
-            System.out.println(kursor.date);
-            kursor = kursor.ref;
-        }
-        System.out.println(kursor.date);
-    }
-    static void add (int date, Node head){
-        Node kursor = head;
-        while(kursor.ref!=null){
-            //System.out.println(kursor.date);
             kursor = kursor.ref;
         }
         Node n = new Node();
@@ -48,4 +15,24 @@ public class Task02 {
         n.date = date;
         kursor.ref = n;
     }
+    void print(Node head){
+        Node kursor = head;
+        while(kursor.ref!=null){
+            System.out.println(kursor.date);
+            kursor = kursor.ref;
+        }
+        System.out.println(kursor.date);
+    }
+}
+public class Task02 {
+    public static void main(String args[]){
+        List l = new List();
+        Node n1 = new Node();
+        l.add(35,n1);
+        l.add(48,n1);
+        l.add(54,n1);
+        l.print(n1);
+        
+        
+    }   
 }
